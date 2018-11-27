ec72c795c16c1f8fe77e0b37296319e49e7b0063
592306832@qq.com <592306832@qq.com>
2018-11-27 22:54:49 +0800

    resultMap

---
+++ mybatis03/src/main/java/com/jiagouedu/mapper/CustomerMapper.java
@@ -0,0 +1,26 @@
+package com.jiagouedu.mapper;
+
+
+import com.jiagouedu.pojo.BatchCustomer;
+import com.jiagouedu.pojo.BatchItem;
+import com.jiagouedu.pojo.Customer;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface CustomerMapper {
+     Customer findCustomerById(int id) throws Exception;
+
+
+    List<BatchCustomer> findBatchCustomer(int id) throws Exception;
+    List<BatchItem> findBatchCustomerToMap(int id) throws Exception;
+
+
+     void insertCustomer(Customer customer) throws Exception;
+      
+
+     void deleteCustomer(int id) throws Exception;
+      
+
+     void updateCustomerAcNo(Customer customer) throws Exception;
+}
--- mybatis03/src/test/java/com.jiagouedu/MybatisTest2.java
+++ mybatis03/src/test/java/com.jiagouedu/MybatisTest2.java
@@ -1,11 +1,10 @@
 package com.jiagouedu;
 
+import com.jiagouedu.mapper.CustomerMapper;
 import com.jiagouedu.mapper.ProductMapper;
 import com.jiagouedu.mapper.ProductMapper1;
 import com.jiagouedu.mapper.UserMapper;
-import com.jiagouedu.pojo.Product;
-import com.jiagouedu.pojo.Reply;
-import com.jiagouedu.pojo.User;
+import com.jiagouedu.pojo.*;
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.Test;
@@ -14,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,6 +26,45 @@ public class MybatisTest2 {
   private ProductMapper productMapper;
   @Autowired
   private ProductMapper1 productMapper1;
+  @Autowired
+  private CustomerMapper customerMapper;
+
+  @Test
+  public void testBatchCustomerToMap() throws Exception{
+
+    //调用userMapper的方法
+    List<BatchItem> bcList=customerMapper.findBatchCustomerToMap(1);
+    if(bcList!=null){
+      BatchItem batchItem = null;
+      Customer customer = null;
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      for (int i = 0; i < bcList.size(); i++) {
+        batchItem = bcList.get(i);//取出批次对象
+        customer = batchItem.getCustomer();//取出该批次的用户信息
+        System.out.println("卡号为"+customer.getAcno()+"的名为"
+                +customer.getUsername()+"的客户:\n于"
+                +sdf.format(batchItem.getCreatetime())+"采购了批次号为"
+                +batchItem.getNumber()+"的一批理财产品");
+      }
+    }
+  }
+  @Test
+  public void testBatchCustomer() throws Exception{
+    //调用userMapper的方法
+    List<BatchCustomer> bcList = customerMapper.findBatchCustomer(1);
+    if(bcList!=null){
+      BatchCustomer batchCustomer;
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      for (int i = 0; i < bcList.size(); i++) {
+        batchCustomer = bcList.get(i);
+        System.out.println("卡号为"+batchCustomer.getAcno()+"的名为"
+                +batchCustomer.getUsername()+"的客户:\n于"
+                +sdf.format(batchCustomer.getCreatetime())+"采购了批次号为"
+                +batchCustomer.getNumber()+"的一批理财产品");
+      }
+    }
+  }
+
   @Test
   public void testUserMapper() {
     User selectUser = userMapper.selectUser(1);
